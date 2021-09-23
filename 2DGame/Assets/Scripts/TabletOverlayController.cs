1f81344afdd5bf1d4dbe4023f882c6db50c67286
PatrickP1992 <42120798+PatrickP1992@users.noreply.github.com>
2021-09-23 12:18:12 +0200

    LightController script erstellt

--- 2DGame/Assets/Scripts/TabletOverlayController.cs
+++ 2DGame/Assets/Scripts/TabletOverlayController.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TabletOverlayController : MonoBehaviour
-{    
-        public GameObject panel;
+{
+    public GameObject panel;
     
-        public void CloseTabletPanel()
-        {
+    public void CloseTabletPanel()
+    {
             if (panel != null)
             {
                 panel.SetActive(false);
             }
-        }
     }
+}
 
