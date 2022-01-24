7e3350a0aee1da5b31d5e172a4f460fa33a70f37
Christian <christiandeutinger@hotmail.com>
2022-01-24 14:00:36 +0100

    fertiges Tutorial in einen Ordner

--- 2DGame/Assets/Scripts/MainMenu.cs
+++ 2DGame/Assets/Scripts/MainMenu.cs
@@ -19,6 +19,6 @@ public class MainMenu : MonoBehaviour
      */
     public void StartGame()
     {
-        SceneManager.LoadScene("__TestSceneUI");
+        SceneManager.LoadScene("TutorialScene");
     }
 }
